940df6149a6be4eb893df86a52edd2b7b4fbeecc
ericlt-dev <ericlt@live.com>
2021-08-03 13:56:29 -0400

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,20 +1,36 @@
     $(document).ready(function() {
         $('div').fadeOut(1);
-        $('div').fadeIn(1000);
+        $('div').fadeIn(200);
     });
+
+    $('.nav-links a').on('click', function(e){
+        if(this.hash !== '') {
+            e.preventDefault();
+    
+            const hash=this.hash;
+            
+            $('html, body').animate(
+                {
+                    scrollTop: $(hash).offset().top
+                },
+                 700
+            );
+        }
+    });
+
  
     $(document).ready(function() {
         const page_url = window.location.href;
         const page_id = page_url.substring(page_url.lastIndexOf("#") + 1);
         if (page_id == "home") {
             $("html, body,").animate({
-                scrollTop: $("#scroll-" + page_id).offset().top - 20
+                scrollTop: $("#scroll-" + page_id).offset().top
             }, 1000);
         } else if (page_id == "about") {
             $("html, body,").animate({
                 scrollTop: $("#scroll-" + page_id).offset().top 
             }, 1000);
-        } else if (page_id == "work") {
+        } else if (page_id == "career") {
         $("html, body,").animate({
             scrollTop: $("#scroll-" + page_id).offset().top 
             }, 1000);
@@ -35,7 +51,7 @@
 
                 var windowheight = window.innerHeight;
                 var revealtop = reveals[i].getBoundingClientRect().top;
-                var revealpoint= 150;
+                var revealpoint= 20;
 
                 if (revealtop < windowheight -revealpoint){
                     reveals[i].classList.add('active');
@@ -45,5 +61,4 @@
                 }
             }
         };
-
         
